5fc959c5997a9b39e2fa76f30ecaa2e29988e3e4
Artem Sitd <tym3@yandex.ru>
2023-11-09 00:41:55 +0500

    исправлено: post, delete /basket

--- basket/views.py
+++ basket/views.py
@@ -1,52 +1,45 @@
 from product.models import Product
-from .serializers import ProductSerializer, BasketApiSerialize
+from .serializers import BasketApiSerialize
 from .models import ItemBasket, TestBasket
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import exceptions
 
-# Третья версия через 2 модели**************************************************************
+
 class BasketApi(APIView):
-    # Полученеи продуктов в корзине
     def get(self, request):
-        user_cart = TestBasket.objects.get(user=request.user.id) # Определяем корзину с
-                                                                    # привязкой к юзеру >>> pwd123
-
-        items_in_basket = ItemBasket.objects.prefetch_related('product')\
-            .filter(cart_id=user_cart.id).values_list('product', flat=True) #Определяем перечень продуктов в корзине.
-                                                        # Выдает объекты модели ItemBasket >>> <QuerySet [4, 2]>
-        product_instance=Product.objects.filter(id__in=items_in_basket) # Получаем объекты !!конкретно!! модели Product (список из id)
-                                    # на выходе кортеж из объектов Product
-                                    # >>>> <QuerySet [<Product: Call of duty 2, id=2>, <Product: Elden Ring, id=4>]>
-        serialized=ProductSerializer(product_instance, many=True)
-
-        # Сериализация через BasketApiSerialize
-        qs=ItemBasket.objects.prefetch_related('product').filter(cart_id=user_cart.id)
-        serialized2=BasketApiSerialize(qs, many=True)
+        user_cart = TestBasket.objects.get(user=request.user.id)  # Определяем корзину с привязкой к юзеру >>> pwd123
+        qs = ItemBasket.objects.prefetch_related('product').filter(cart_id=user_cart.id)
+        serialized2 = BasketApiSerialize(qs, many=True)
         return Response(data=serialized2.data, status=200)
-        # return Response(data=serialized.data, status=200)
 
     # Добавление продукта
     def post(self, request):
-        # НЕОБХОДИМО ПЕРЕДАВАТЬ НЕ ПРОДУКТЫ, А ВСЕ ОБЪЕКТЫ ItemBasket, ЧТОБЫ ДОБРАТЬСЯ ДО ПРАВИЛЬНОГО COUNT!!
         user_cart = TestBasket.objects.get(user=request.user.id)
         product = Product.objects.get(id=request.data['id'])
-        items_in_basket = ItemBasket.objects.filter(cart=user_cart.id)
+        items_in_basket = ItemBasket.objects.prefetch_related('product').filter(cart=user_cart.id)
         if items_in_basket.filter(product__id=request.data['id']).exists():
-            raise exceptions.ParseError('Object already in basket')
+            temp = items_in_basket.get(product=product)
+            temp.basket_count += request.data['count']
+            temp.save()
+            serialized = BasketApiSerialize(items_in_basket, many=True)
+            return Response(data=serialized.data, status=200)
         else:
             data = ItemBasket.objects.create(cart=user_cart, product=product,
                                              basket_count=request.data['count'])
-            serialized = ProductSerializer(data.product)
+            serialized = BasketApiSerialize(data)
             return Response(data=serialized.data, status=200)
 
     # Удаление продукта из корзины
     def delete(self, request):
-        #НУЖНО ПЕРЕДЕЛАТЬ ЛОГИКУ УДАЛЕНИЯ, СРАВНИВАЯ COUNT ИЗ REQUEST.DATA['COUNT'] И BASKET_COUNT
-        # ПРИ УМНЕЬШЕНИИ УОЛИЧЕСТВА - НЕ НАДО УДАЛЯТЬ ВЕСЬ ПРОДУКТ!!!!
-        print(request.data)
         user_cart = TestBasket.objects.get(user=request.user.id)
         product = Product.objects.get(id=request.data['id'])
-        ItemBasket.objects.filter(cart=user_cart.id).get(product=product.id).delete()
-        serialized = BasketApiSerialize(product)
+        item_change = ItemBasket.objects.filter(cart=user_cart.id).get(product=product.id)
+
+        if request.data['count'] >= item_change.basket_count:
+            item_change.delete()
+            serialized = BasketApiSerialize(product)
+            return Response(data=serialized.data, status=200)
+        item_change.basket_count -= request.data['count']
+        item_change.save()
+        serialized = BasketApiSerialize(item_change)
         return Response(data=serialized.data, status=200)
--- product/admin.py
+++ product/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import Product, Reviews, Specifications, ProductImage, Sales
 
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'price', 'count', 'rating', 'limited', 'category')
 
 
 @admin.register(Reviews)
 class ReviewsAdmin(admin.ModelAdmin):
     list_display = ("author", 'product')
+
+
 admin.site.register(Specifications)
 admin.site.register(ProductImage)
 admin.site.register(Sales)
